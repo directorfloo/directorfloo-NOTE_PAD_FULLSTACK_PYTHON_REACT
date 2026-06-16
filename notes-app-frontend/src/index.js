ac2683043f922da7ed33b2a9da6e45ac1aea2877
folorunsho olamide <folorunsho891@gmal.com>
2026-06-16 08:26:04 +0100

    index

--- notes-app-frontend/src/index.js
+++ notes-app-frontend/src/index.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -6,9 +7,9 @@ import { store } from "./store/store.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
 );
