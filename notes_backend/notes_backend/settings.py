37cdffe9f56e77d848b6ec489d5b7c362265d985
folorunsho olamide <folorunsho891@gmal.com>
2026-06-17 01:23:10 +0100

    note

--- notes_backend/notes_backend/settings.py
+++ notes_backend/notes_backend/settings.py
@@ -131,6 +131,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "https://pourmymindnoteapp.netlify.app",
+    "https://pourmymindnoteapp.netlify.app/register"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
